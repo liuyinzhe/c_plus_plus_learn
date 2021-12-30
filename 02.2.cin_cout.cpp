bb3f7f7df1d0543a6c080215e470fd95414f1d95
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2021-12-30 15:02:49 +0800

    Create 02.2.cin_cout.cpp

---
+++ 02.2.cin_cout.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    cout << "hellworld" << endl;
+    //32 2[01], 4bytes 8bit
+    ////内存,内存名字
+    int wordCnt;
+    int personCnt;
+    int roomCnt;
+    wordCnt = personCnt = roomCnt = 10;
+
+    cin >> wordCnt;
+    cout << "word count: " << wordCnt << "room count: " << roomCnt << endl;
+
+    double a = sqrt(10.0);
+    cout << "sqrt result: " << a <<endl;
+    return 0;
+}
